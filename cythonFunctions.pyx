4c34ab350c57034e9fddbb2cb1a938434f25cab3
Landan MacDonald <landanmacdonald@gmail.com>
2015-02-10 10:17:29 -0700

    starting to add some Cython

---
+++ cythonFunctions.pyx
@@ -0,0 +1,11 @@
+
+def bestxy(emislat,emislon,lat,lon):
+   bestfit=1e20
+   for x in range(0,349):
+      for y in range(0,277):
+         thisfit = (lat[y,x]-emislat)**2 + (lon[y,x]-emislon)**2
+         if thisfit < bestfit:
+            bestfit = thisfit
+            bestx = x
+            besty = y
+   return bestx,besty
--- readReanalysis.py
+++ readReanalysis.py
@@ -4,17 +4,17 @@ import numpy as np
 import datetime
 from scipy.interpolate import LinearNDInterpolator, griddata, interp1d
 import sys
-
-def bestxy(emislat,emislon,lat,lon):
-   bestfit=1e20
-   for x in range(0,349):
-      for y in range(0,277):
-         thisfit = (lat[y,x]-emislat)**2 + (lon[y,x]-emislon)**2
-         if thisfit < bestfit:
-            bestfit = thisfit
-            bestx = x
-            besty = y
-   return bestx,besty
+from cythonFunctions import bestxy
+# def bestxy(emislat,emislon,lat,lon):
+#    bestfit=1e20
+#    for x in range(0,349):
+#       for y in range(0,277):
+#          thisfit = (lat[y,x]-emislat)**2 + (lon[y,x]-emislon)**2
+#          if thisfit < bestfit:
+#             bestfit = thisfit
+#             bestx = x
+#             besty = y
+#    return bestx,besty
 
 def readSfcReanalysis(fname,keyword,year,lat,lon,t):
 
